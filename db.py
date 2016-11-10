52b0e19539f3d6f4fe412ca096e6b90ec26c34cf
Ranuka Perera <random@sawrc.com>
2016-11-10 06:49:09 +0530

    test

--- db.py
+++ db.py
@@ -1,3 +1,6 @@
+import functools
+from contextlib import contextmanager
+
 from sqlalchemy import Column, Integer, Boolean, DateTime, Date, Time, Float, String, create_engine
 from sqlalchemy.dialects.sqlite import BLOB, BOOLEAN, CHAR, DATE, DATETIME, DECIMAL, FLOAT, INTEGER, REAL, NUMERIC, SMALLINT, TIME, TIMESTAMP, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
@@ -38,11 +41,43 @@ class Persistence(object):
         self.Session = sessionmaker(bind=db_engine)
         self.create_database()
 
+    # def session_scope(self):
+    #     """Provide a transactional scope around a series of operations."""
+    #     session = self.Session()
+    #     try:
+    #         yield session
+    #         session.commit()
+    #     except:
+    #         session.rollback()
+    #         raise
+    #     finally:
+    #         session.close()
+
+    def session_scope(func):
+        """Decorator which wraps the decorated function in a transactional session. If the
+           function completes successfully, the transaction is committed. If not, the transaction
+           is rolled back."""
+
+        # @functools.wraps(func)
+        def outer_wrapper(*args, **kwargs):
+
+            session = args[0].Session()
+            try:
+                yield func
+                session.commit()
+            except:
+                session.rollback()
+                raise
+            finally:
+                session.close()
+
+        return outer_wrapper
+
     @classmethod
     def create_database(cls):
         metadata.create_all(db_engine, checkfirst=True)
 
-    def add_record(self, record):
+    def add_record(self, record, session):
         session = self.Session()
         if hasattr(record, '__iter__'):
             session.add_all(record)
@@ -50,6 +85,14 @@ class Persistence(object):
             session.add(record)
         session.commit()
 
-    def get_attribute(self, name):
-        session = self.Session()
-        return session.query(ATTR.value).filter(ATTR.name == name).one()
+    @session_scope
+    def get_count(self, session=None):
+        return session.filter(RECORD).count()
+
+    def get_attribute(self, name, session):
+        return session.query(ATTR.value).filter(ATTR.name == name).first()
+
+    @property
+    @session_scope
+    def is_first_run(self, session):
+        return session
---
+++ test.py
@@ -0,0 +1,4 @@
+from db import Persistence
+
+persistence = Persistence()
+persistence.get_count()
