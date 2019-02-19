db1fed32a6d60a5307d59a6fa5cf809116964527
Ranuka Perera <random@sawrc.com>
2019-02-19 13:32:53 +0530

    * Add date range test

--- ilocate/__main__.py
+++ ilocate/__main__.py
@@ -35,18 +35,6 @@ def main():
     else:
         runner.daily_update()
 
-    """{'speed': 0, 'dist_from_last': 0, 'state': 'on', 'lon': '7.8890638', 'time_from_last': 0,
-    'nic': None, 'lat': '7.0597020', 'timestamp': 1467524518, 'device_type': '9', 'charge_status': '1', 'id': '1518',
-    'number': '77xxxxxxx', 'time_st': 'July 3, 2016, 11:11 AM', 'error': False, 'update_type': '2', 'name': 'XXX-0000'}"""
-
-    # Read config
-    # Check if first
-    # If first, find earliest
-    ## get data from earliest to now
-    # If not first
-    ## Get data from last to now
-    # Exit
-
 
 if __name__ == "__main__":
     main()
--- ilocate/tests/test_runner.py
+++ ilocate/tests/test_runner.py
@@ -13,7 +13,16 @@ class TestRunner(unittest.TestCase):
         @staticmethod
         def date_data(dt):
             """Function to serve the preset data based on the input date"""
-            if dt.date() == date(2018, 11, 6):
+            if dt.date() == date(2018, 11, 7):
+                return [
+                    {
+                        'lon': '79.8887663', 'time_st': 'November 7, 2018, 05:12:00 AM', 'timestamp': 1541547720,
+                        'dist_from_last': 0, 'id': '1518', 'error': False, 'speed': 0, 'time_from_last': 0, 'device_type': '9',
+                        'nic': None, 'charge_status': '1', 'number': '778352958', 'lat': '7.0598198', 'name': 'CAE-6745',
+                        'update_type': '2', 'state': 'on'
+                    },
+                ]
+            elif dt.date() == date(2018, 11, 6):
                 return [
                     {
                         'lon': '79.8887663', 'time_st': 'November 6, 2018, 06:12:58 PM', 'timestamp': 1541508178,
@@ -43,9 +52,35 @@ class TestRunner(unittest.TestCase):
                 return None
         cls.date_data = date_data
 
+        @staticmethod
+        def date_multi_data(dt1, dt2):
+            return [
+                {
+                    'lon': '79.8887663', 'time_st': 'November 6, 2018, 06:12:58 PM', 'timestamp': 1541508178,
+                    'dist_from_last': 0, 'id': '1518', 'error': False, 'speed': 0, 'time_from_last': 0, 'device_type': '9',
+                    'nic': None, 'charge_status': '1', 'number': '778352958', 'lat': '7.0598198', 'name': 'CAE-6745',
+                    'update_type': '2', 'state': 'on'
+                },
+                {
+                    'lon': '79.8893412', 'time_st': 'November 6, 2018, 06:15:59 PM', 'timestamp': 1541508359,
+                    'dist_from_last': 0.06556343164903, 'id': '1518', 'error': False, 'speed': 1.3040240548978,
+                    'time_from_last': 181, 'device_type': '9', 'nic': None, 'charge_status': '1', 'number': '778352958',
+                    'lat': '7.0596710', 'name': 'CAE-6745', 'update_type': '2', 'state': 'on'
+                },
+                {
+                    'lon': '79.8887663', 'time_st': 'November 5, 2018, 01:12:00 AM', 'timestamp': 1541360520,
+                    'dist_from_last': 0, 'id': '1518', 'error': False, 'speed': 0, 'time_from_last': 0,
+                    'device_type': '9', 'nic': None, 'charge_status': '1', 'number': '778352958',
+                    'lat': '7.0598198', 'name': 'CAE-6745', 'update_type': '2', 'state': 'on'
+                },
+            ]
+
+        cls.date_multi_data = date_multi_data
+
     def setUp(self):
         api = Mock()
         api.get_data = Mock(side_effect=self.date_data)
+        api.get_data_range = Mock(side_effect=self.date_multi_data)
         persistence = Mock()
         self.yesterday = datetime.now() - timedelta(days=1)
         type(persistence).latest_record_datetime = PropertyMock(return_value=self.yesterday)
@@ -64,8 +99,13 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(self.runner.persistence.add_record.call_count, 2)
         self.assertGreaterEqual(self.runner.api.login.call_count, 1)
 
-    def test_save_date_range(self):
-        raise NotImplementedError
+    @patch('ilocate.runner.dict_to_record_objects')
+    def test_save_date_range(self, dict_to_rec):
+        self.runner.retrieve_and_save_date_range(datetime(2018, 11, 5), datetime(2018, 11, 6))
+        self.assertEqual(dict_to_rec.call_count, 3)
+        self.assertEqual(self.runner.persistence.add_record.call_count, 1)
+        self.assertGreaterEqual(self.runner.api.login.call_count, 1)
+
 
     @patch('ilocate.runner.Runner.retrieve_and_save_date_range')
     def test_daily_update(self, save_date_range_mock):
